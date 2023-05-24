0e205ee492384952f49bc3846f5c617d6522100a
“DeoreAnkitaRahul” <ankitapatil375@gmail.com>
2023-05-24 17:47:52 +0530

    [Ankita] Add : UC1-Create Simple LinkedList

---
+++ Day6-PracticeProblem/Day6-PracticeProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Day6_PracticeProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Linked List Program");
+            LinkedList linked = new LinkedList();
+            linked.Add(56);
+            linked.Add(30);
+            linked.Add(70);
+
+            linked.Display();
+        }
+    }                                 
+}
+
+
+
+
